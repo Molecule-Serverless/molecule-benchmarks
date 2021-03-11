d81accd35151e8b78da5d14058ae061339c7134d
Dong Du <ddnirvana1@gamil.com>
2021-03-11 21:55:20 +0800

    single-func: add test results (alexa on smart computer host)
    Signed-off-by: Dong Du <ddnirvana1@gamil.com>

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,2 @@
-docker run --rm -it -v $(pwd):/home -v $(pwd)/../env-javascript/src/:/env --entrypoint=/bin/bash registry.cn-shanghai.aliyuncs.com/jointfaas-serverless/env-javascript:v6.1
+docker run --rm -it -v /home/dd/molecule-faas/molecule-benchmarks:/home -v /home/dd/molecule-faas/molecule-js-env/src:/env --entrypoint=/bin/bash registry.cn-shanghai.aliyuncs.com/jointfaas-serverless/env-javascript:v6.1
+#docker run --rm -it -v /home/dd/molecule-faas/molecule-benchmarks:/home --entrypoint=/bin/bash registry.cn-shanghai.aliyuncs.com/jointfaas-serverless/env-javascript:v6.1
