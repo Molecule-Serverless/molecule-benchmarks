8aa83c7c34d7d29f40a5bfc58818ab8e25c43410
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-23 22:52:36 +0800

    fpga-app: refine scripts
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- fpga-apps/run_bench.sh
+++ fpga-apps/run_bench.sh
@@ -2,11 +2,20 @@
 
 sudo rm -rf test_env
 mkdir test_env
-cd test_env && mkdir -p rootfs
+cd test_env
+
+mkdir -p rootfs
 sudo docker export `docker create ddnirvana/molecule-ae-fpga-bench:latest` | sudo tar -C ./rootfs -xf -
 
 cp ../scripts/gzip_small_config.json ./config.json
 
+
+cp ../gzip_app/small_file.txt ./
+cp ../gzip_app/mid_file.txt ./
+cp ../gzip_app/huge_file.txt ./
+cp ../gzip_app/linux-5.10.55.tar.xz ./
+
+../scripts/eval_gzip.sh
 
 #cd gzip_app && ./eval.sh && cd ..
 #cd anti_money/anti_money_genbin && ../eval.sh && cd ../..
