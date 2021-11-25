d405da70a5c538eb79b61de4e2f1608119386e7d
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-25 14:53:33 +0800

    scripts: use updated image
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,7 @@
 #docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/src:/env --entrypoint=/bin/bash ddnirvana/molecule-js-env:arm-v2
 
 ## For x86 env, uncomment the following command
-docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/src:/env --entrypoint=/bin/bash registry.cn-shanghai.aliyuncs.com/jointfaas-serverless/env-javascript:v6.1
+docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/src:/env --entrypoint=/bin/bash ddnirvana/hclouid-env-javascript:v6.1
 
 
 
