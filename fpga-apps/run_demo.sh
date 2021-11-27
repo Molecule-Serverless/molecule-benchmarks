70ecc835b7410838c635d9aed3f9ee6ebd7d36d0
Dong Du <Dd_nirvana@sjtu.edu.cn>
2021-11-27 05:46:46 +0000

    fpga-app: simplify demo
    Signed-off-by: Dong Du <Dd_nirvana@sjtu.edu.cn>

---
+++ fpga-apps/run_demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo rm -rf test_env
+mkdir test_env
+cd test_env
+
+mkdir -p rootfs
+sudo docker export `sudo docker create ddnirvana/molecule-ae-fpga-bench:latest` | sudo tar -C ./rootfs -xf -
+
+cp ../helloworld/helloworld_config.json ./config.json
+
+
+cp ../helloworld/func_invoker ./
+cp ../helloworld/fpga_func.awsxclbin ./
+
+../../../vsandbox-runtime/vsandbox run demo-sandbox
