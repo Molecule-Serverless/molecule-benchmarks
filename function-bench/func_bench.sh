940dc50d71aacc2e1d91602718d8bb33178f036b
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-30 23:13:18 +0800

    scripts refine: more user friendly
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- function-bench/func_bench.sh
+++ function-bench/func_bench.sh
@@ -5,7 +5,7 @@
 ## Arg2: test-path
 function run_test(){
 	echo ""
-	echo -e "\tTest-Case: $1 (taking from minutes to tens of minutes)"
+	echo -e "\tTest-Case: $1 (taking 5-10 minutes)"
 	./app_template_build_prebuilt.sh ddnirvana/molecule-ae-python-base-image #> /dev/null 2>&1
 	./app_base_build_prebuilt.sh ddnirvana/molecule-ae-app-$2-base #> /dev/null 2>&1
 	./app_endpoint_build_prebuilt.sh ddnirvana/molecule-ae-app-$2-ep #> /dev/null 2>&1
@@ -17,7 +17,7 @@ function run_test(){
 
 function run_test_long(){
 	echo ""
-	echo -e "\tTest-Case: $1 (taking tens of minutes! please be patient!)"
+	echo -e "\tTest-Case: $1 (taking about 30 minutes! please be patient!)"
 	./app_template_build_prebuilt.sh ddnirvana/molecule-ae-python-base-image #> /dev/null 2>&1
 	./app_base_build_prebuilt.sh ddnirvana/molecule-ae-app-$2-base #> /dev/null 2>&1
 	./app_endpoint_build_prebuilt.sh ddnirvana/molecule-ae-app-$2-ep #> /dev/null 2>&1
@@ -25,6 +25,45 @@ function run_test_long(){
 	./run_fork.sh > /dev/null 2>&1
 	python3 ./test_app_long.py
 	#python3 ./test_app.py baseline
+}
+
+
+function run_test_friendly() {
+	echo ""
+	run_test $1 $2 &
+	pid=$! # Process Id of the previous running command
+
+	spin='-\|/'
+	sleep 1
+
+	i=0
+	while kill -0 $pid 2>/dev/null
+	do
+		i=$(( (i+1) %4 ))
+	    printf "\033[46;37m \r[${spin:$i:1}] Running\033[0m  "
+		sleep .1
+	done
+	sleep 2
+	echo -e "\033[42;37m Brilliant! Test case succeed!\033[0m"
+}
+
+function run_test_long_friendly() {
+	run_test_long $1 $2 &
+	pid=$! # Process Id of the previous running command
+
+	spin='-\|/'
+
+	i=0
+	sleep 1
+	while kill -0 $pid 2>/dev/null
+	do
+		i=$(( (i+1) %4 ))
+	    printf "\033[46;37m \r[${spin:$i:1}] Running\033[0m  "
+		sleep .1
+	done
+	sleep 2
+	echo -e "\033[42;37m Brilliant! Test case succeed!\033[0m"
+
 }
 
 echo "Function-bench Tests"
@@ -37,12 +76,12 @@ mkdir -p ~/.base/container0
 mkdir -p ~/.base/spin0
 mkdir -p ~/.base/baseline
 
-run_test LinPack linpack
-run_test Chameleon chameleon
-run_test Matmul matmul
-run_test PyAES pyaes
-run_test DD dd
-run_test Gzip-comprssion gzip-compression
-run_test_long Video-Processing video-processing
+run_test_friendly LinPack linpack
+run_test_friendly Chameleon chameleon
+run_test_friendly Matmul matmul
+run_test_friendly PyAES pyaes
+run_test_friendly DD dd
+run_test_friendly Gzip-comprssion gzip-compression
+run_test_long_friendly Video-Processing video-processing
 
 popd
