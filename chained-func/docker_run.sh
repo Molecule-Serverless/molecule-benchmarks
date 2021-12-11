252ba9118a62902f6228793d295bc91f1235c786
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-11 21:03:49 +0800

    minors
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- chained-func/docker_run.sh
+++ chained-func/docker_run.sh
@@ -3,7 +3,7 @@ pushd $(pwd)/../molecule-js-env/ipc-dag-src/tests/ipc/chain/ > /dev/null
 echo -e "\033[42;37m Chained Function Tests\033[0m"
 echo -e "\033[42;37m (1) Molecule's Results\033[0m"
 ./run_chain.sh
-echo -e "\033[42;37m (1) Baseline's Results\033[0m"
+echo -e "\033[42;37m (2) Baseline's Results\033[0m"
 ./run_chain_baseline.sh
 echo -e "\033[42;37m Chained Function Tests Finished\033[0m"
 popd > /dev/null
