873157661e864986958bfbe7ba76a547f49a8fed
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-11 20:33:58 +0800

    chained apps: add baseline data now
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- chained-func/docker_run.sh
+++ chained-func/docker_run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 pushd $(pwd)/../molecule-js-env/ipc-dag-src/tests/ipc/chain/ > /dev/null
+echo -e "\033[42;37m Chained Function Tests\033[0m"
+echo -e "\033[42;37m (1) Molecule's Results\033[0m"
 ./run_chain.sh
+echo -e "\033[42;37m (1) Baseline's Results\033[0m"
+./run_chain_baseline.sh
+echo -e "\033[42;37m Chained Function Tests Finished\033[0m"
 popd > /dev/null
