45ec11d04a5f8bd4e3e84c77ff27ad126563cf84
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-13 20:00:26 +0800

    staged: add baseline test
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- staged-func/docker_run.sh
+++ staged-func/docker_run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 pushd $(pwd)/../molecule-js-env/ipc-dag-src/tests/ipc/stages/ > /dev/null
+echo -e "\033[42;37m Staged Function Tests\033[0m"
+echo -e "\033[42;37m (1) Molecule's Results\033[0m"
 ./run_alexa_stage_tests.sh -a
+echo -e "\033[42;37m (2) Baseline's Results\033[0m"
+./run_alexa_stage_tests.sh -b
+echo -e "\033[42;37m Staged Function Tests Finished\033[0m"
 popd > /dev/null
