6bde539fb10a9756e68111557c7cb694385fafd9
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-14 14:43:55 +0800

    arm: minor updates to use latest version of docker images
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- single-func/arm_docker_run.sh
+++ single-func/arm_docker_run.sh
@@ -1,5 +1,6 @@
 ## For ARM env, uncomment the following command
-docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/single-func-src:/env -w /env/ --entrypoint=/env/test_single_func_exec.sh ddnirvana/molecule-js-env:arm-v2
+docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/single-func-src:/env -w /env/ --entrypoint=/env/test_single_func_exec.sh ddnirvana/molecule-js-env:v3-node14.16.0-arm
+
 
 ## For x86 env, uncomment the following command
 #docker run --rm -it -v $(pwd):/home -v $(pwd)/../molecule-js-env/single-func-src:/env -w /env/ --entrypoint=/env/test_single_func_exec.sh ddnirvana/hclouid-env-javascript:v6.1
