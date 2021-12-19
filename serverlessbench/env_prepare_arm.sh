20ff9f2576eb5f31fba4af0498f0e0d173de93a9
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-12-19 14:57:51 +0800

    arm: add arm-relatesd scripts
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- serverlessbench/env_prepare_arm.sh
+++ serverlessbench/env_prepare_arm.sh
@@ -5,29 +5,26 @@
 
 function prepare_img(){
 	pushd $2 > /dev/null
-	docker build -f ./Dockerfile-arm64 -t ddnirvana/molecule-ae-app-$1-base:latest-arm .
+
+	cd baseline-image
+	docker build -t -f ./Dockerfile-arm64 ddnirvana/molecule-ae-app-$1-base:latest-arm .
 	docker push ddnirvana/molecule-ae-app-$1-base:latest-arm
 
+	cd ../spin-base-image
 	gcc spin.c -o spin -O2 -static
 	docker build -f ./Dockerfile-arm64 -t ddnirvana/molecule-ae-app-$1-ep:latest-arm .
 	docker push ddnirvana/molecule-ae-app-$1-ep:latest-arm
 
 	popd > /dev/null
-
 }
 
-echo "Preparing images for Function-bench Tests"
 
-prepare_img linpack ../../functionBench/molecule/linpack
-prepare_img chameleon ../../functionBench/molecule/chameleon
-prepare_img matmul ../../functionBench/molecule/matmul
-prepare_img pyaes ../../functionBench/molecule/pyaes
-prepare_img video-processing ../../functionBench/molecule/video_processing
-prepare_img dd ../../functionBench/molecule/dd
-prepare_img gzip-compression ../../functionBench/molecule/gzip_compression
+echo "Preparing images for Serverlessbench Tests"
+
+prepare_img image-resize ../../forkable-python-runtime
 
 ## Prepare template image
-pushd ../../functionBench/molecule/python-template
-docker build -f Dockerfile-arm64 -t ddnirvana/molecule-ae-python-base-image:latest-arm .
-docker push ddnirvana/molecule-ae-python-base-image:latest-arm
+pushd ../../forkable-python-runtime/python-base-image
+docker build -f Dockerfile-arm64 -t ddnirvana/molecule-ae-serverlessbench-python-base-image:latest-arm .
+docker push ddnirvana/molecule-ae-serverlessbench-python-base-image:latest-arm
 popd
